d8192de6ad88fec862aa9a55fdd959c4cdc46ce5
Jio <jioooo@pm.me>
2025-10-25 14:29:36 -0400

    Update recur_py_v1.py. Rename coming in next commit

--- add two numbers/recur_py_v1.py
+++ add two numbers/recur_py_v1.py
@@ -6,27 +6,23 @@ class ListNode:
 
 # My code starts here v
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry=0) -> Optional[ListNode]:
-        l3 = ListNode(val=l1.val + l2.val + carry)
+    def add_two_numbers(self, l1: ListNode | None, l2: ListNode | None, carry=0) -> ListNode:
+        l3 = ListNode(val = l1.val + l2.val + carry)
 
         if l3.val >= 10:
             l3.val -= 10
             carry = 1
-        else:
-            carry = 0
+        else: carry = 0
 
-        if l1.next == None and l2.next != None:
-            l3.next = self.addTwoNumbers(ListNode(), l2.next, carry)
-        elif l2.next == None and l1.next != None:
-            l3.next = self.addTwoNumbers(l1.next, ListNode(), carry)
+        if l1.next is None and l2.next is not None:
+            l3.next = self.add_two_numbers(ListNode(), l2.next, carry)
+        elif l2.next is None and l1.next is not None:
+            l3.next = self.add_two_numbers(l1.next, ListNode(), carry)
 
-        if carry == 1 and l1.next == None and l2.next == None and l1.val + l2.val + carry >= 10:
+        if carry == 1 and l1.next is None and l2.next is None and l1.val + l2.val + carry >= 10:
             l3.next = ListNode(val=1)
 
-        if l1.next != None and l2.next != None:
-            l3.next = self.addTwoNumbers(l1.next, l2.next, carry)
-
-        carry = 0
+        if l1.next is not None and l2.next is not None:
+            l3.next = self.add_two_numbers(l1.next, l2.next, carry)
 
         return l3
-
